5a4363732bd1c11ed9e912cb6ed9fdd2a0cf6fc8
StevenMX <alab01845@gmail.com>
2026-04-25 20:32:09 -0300

    fix

--- build_turnip.sh
+++ build_turnip.sh
@@ -40,6 +40,7 @@ compile_mesa() {
     sed -i 's/typedef const native_handle_t\* buffer_handle_t;/typedef void\* buffer_handle_t;/g' include/android_stub/cutils/native_handle.h || true
     sed -i 's/, hnd->handle/, (void \*)hnd->handle/g' src/util/u_gralloc/u_gralloc_fallback.c || true
     sed -i 's/native_buffer->handle->/((const native_handle_t \*)native_buffer->handle)->/g' src/vulkan/runtime/vk_android.c || true
+    sed -i 's/anb->handle->/((const native_handle_t \*)anb->handle)->/g' src/vulkan/runtime/vk_android.c || true
 
     mkdir -p subprojects && cd subprojects
     rm -rf spirv-tools spirv-headers
